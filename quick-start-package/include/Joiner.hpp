c48e3fb2d24e90c96ca6cd84a9d4bfb869799ce5
Orest Gkini <orestijuve@gmail.com>
2018-03-01 19:17:47 +0200

    Fixed indentation

--- quick-start-package/include/Joiner.hpp
+++ quick-start-package/include/Joiner.hpp
@@ -3,18 +3,18 @@
 #include <cstdint>
 #include "Relation.hpp"
 #include "Parser.hpp"
-//---------------------------------------------------------------------------
-//---------------------------------------------------------------------------
-class Joiner {
-  /// The relations that might be joined
-  std::vector<Relation> relations;
 
-  public:
-  /// Add relation
-  void addRelation(const char* fileName);
-  /// Get relation
-  Relation& getRelation(unsigned id);
-  /// Joins a given set of relations
-  void join(QueryInfo& i);
+class Joiner {
+    std::vector<Relation> relations; // The relations that might be joined
+
+    public:
+        
+    // Add relation
+    void addRelation(const char* fileName);
+    
+    // Get relation
+    Relation& getRelation(unsigned id);
+    
+    // Joins a given set of relations
+    void join(QueryInfo& i);
 };
-//---------------------------------------------------------------------------
