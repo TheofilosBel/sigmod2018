bce8469e78d7ed65828922563a1dbfe01d78517b
Orest Gkini <orestijuve@gmail.com>
2018-03-03 11:41:02 +0200

    Changed harness.cpp for debugging - deleted build

--- quick-start-package/build/release/CMakeFiles/link_target.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/link_target"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/link_target.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
