6e485f1cf90c25c5ea6a182d3ec570faeef5ad0f
yorgosk <kgeoinvest@gmail.com>
2018-03-04 18:38:31 +0200

    compiles

--- quick-start-package/Joiner.cpp
+++ quick-start-package/Joiner.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include "Parser.hpp"
 #include "QueryGraph.hpp"
+#include "execution_plan/include/header.hpp"
 
 using namespace std;
 
@@ -244,7 +245,7 @@ table_t* Joiner::low_join(table_t *table_r, table_t *table_s) {
                 /* get the first values from the r's rows ids */
                 for (uint64_t j = 0 ; j < h_rows.size(); j++)
                     update_row_ids[j].push_back(h_rows[j][vals->second.index]);
-               
+
                 /* then go to the s's row ids to get the values */
                 for (uint64_t j = 0; j < i_rows.size(); j++)
                     update_row_ids[j + h_rows.size()].push_back(i_rows[j][i]);
@@ -274,7 +275,7 @@ table_t* Joiner::low_join(table_t *table_r, table_t *table_s) {
         updated_table_t->relations_row_ids = new std::vector<std::vector<int>>;
         updated_table_t->relations_row_ids->resize(h_rows.size()+i_rows.size(), std::vector<int>());
         std::vector<std::vector<int>> &update_row_ids = *updated_table_t->relations_row_ids;
-        
+
         /* now the phase of hashing */
         for (uint64_t i = 0; i < iter_size; i++) {
             /* remember we may have multi vals in 1 key,if it isnt a primary key */
@@ -412,14 +413,7 @@ int main(int argc, char* argv[]) {
         // Parse the query
         i.parseQuery(line);
 
-        // Fill the query graph
-        queryGraph.fill(i.predicates);
-
-        // Join the relations
-        joiner.join(i);
-
-        // Clear the graph for the next query
-        queryGraph.clear();
+        JTree *jTreePtr = treegen(&i);
     }
 
     return 0;
