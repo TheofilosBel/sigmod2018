8cdf085c1b3e4c6da15167c44b5cc77a2de6fb19
yorgosk <kgeoinvest@gmail.com>
2018-03-11 00:33:56 +0200

    some additions

--- quick-start-package/QueryPlan.cpp
+++ quick-start-package/QueryPlan.cpp
@@ -2,23 +2,33 @@
 
 /* construct a Query Plan -- do all the pre-processing that is necessary for a
   Query to be translated into a Join Tree and then start the whole process */
-void constrQueryPlan(QueryInfo* queryInfoPtr) {
+QueryPlan* constrQueryPlan(QueryInfo* queryInfoPtr) {
+  QueryPlan* queryPlanPtr = malloc(1 * sizeof(QueryPlan));
+
+  /* somehow collect relationships stats */
+
   /* create an array of table-types from the query relations */
 
   /* apply all filters */
-  for (std::vector<int>::iterator it = queryInfoPtr->filters.begin() ; it != queryInfoPtr->filters.end(); ++it) {
+  for (std::vector<int>::iterator it = queryInfoPtr->filters.begin(); it != queryInfoPtr->filters.end(); ++it) {
     //
   }
 
   /* create an array of sets of relations to be joined */
 
-  /* create an array of Join Trees for each set of relations */
+  /* create an array of Join Trees, one for each set of relations */
+  std::vector<JoinTree> joinTreeVec;
 
   /* merge all Join Trees to one final Join Tree */
+  JoinTree* tempJoinTreePtr = joinTreeVec.begin();
+  for (std::vector<JoinTree>::iterator it = std::next(joinTreeVec.begin()); it != joinTreeVec.end(); ++it) {
+    tempJoinTreePtr = constrJoinTreeFromJoinTrees(tempJoinTreePtr, *it);
+  }
+  queryPlanPtr->joinTreePtr = tempJoinTreePtr;
 }
 
 /* construct an optimal left-deep Join Tree from a given set of relations */
-JoinTree* constrJoinTreeFromRelations() {
+JoinTree* constrJoinTreeFromRelations(std::vector<Relation>& relations) {
   return NULL;
 }
 
@@ -27,8 +37,18 @@ JoinTree* constrJoinTreeFromJoinTrees(JoinTree* joinTreePtr1, JoinTree* joinTree
   return NULL;
 }
 
+/* construct a Join Tree node */
+JoinTreeNode* constrJoinTreeNode() {
+  return NULL;
+}
+
 /* destruct a Join Tree properly */
 void destrJoinTree(JoinTree* joinTreePtr) {
+  ;
+}
+
+/* destruct a Join Tree node properly */
+void destrJoinTreeNode(JoinTreeNode* joinTreeNodePtr) {
   ;
 }
 
@@ -37,7 +57,17 @@ void printJoinTree(JoinTree* joinTreePtr) {
   ;
 }
 
+/* print a Join Tree node -- for debugging */
+void printJoinTreeNode(JoinTreeNode* joinTreeNodePtr) {
+  ;
+}
+
 /* estimate the cost of a given Join Tree */
 double joinTreeCost(JoinTree* joinTreePtr) {
+  return 1.0;
+}
+
+/* estimate the cost of a given Join Tree node */
+double joinTreeNodeCost(JoinTreeNode* joinTreeNodePtr) {
   return 1.0;
 }
--- quick-start-package/include/QueryPlan.hpp
+++ quick-start-package/include/QueryPlan.hpp
@@ -30,19 +30,31 @@ typedef struct QueryPlan {
 
 /* construct a Query Plan -- do all the pre-processing that is necessary for a
   Query to be translated into a Join Tree and then start the whole process */
-void constrQueryPlan(QueryInfo* queryInfoPtr);
+QueryPlan* constrQueryPlan(QueryInfo* queryInfoPtr);
 
 /* construct an optimal left-deep Join Tree from a given set of relations */
-JoinTree* constrJoinTreeFromRelations();
+JoinTree* constrJoinTreeFromRelations(std::vector<Relation>& relations);
 
 /* construct an optimal Join Tree from two given optimal Join Trees */
 JoinTree* constrJoinTreeFromJoinTrees(JoinTree* joinTreePtr1, JoinTree* joinTreePtr2);
 
+/* construct a Join Tree node */
+JoinTreeNode* constrJoinTreeNode();
+
 /* destruct a Join Tree properly */
 void destrJoinTree(JoinTree* joinTreePtr);
+
+/* destruct a Join Tree node properly */
+void destrJoinTreeNode(JoinTreeNode* joinTreeNodePtr);
 
 /* print a Join Tree -- for debugging */
 void printJoinTree(JoinTree* joinTreePtr);
 
+/* print a Join Tree node -- for debugging */
+void printJoinTreeNode(JoinTreeNode* joinTreeNodePtr);
+
 /* estimate the cost of a given Join Tree */
 double joinTreeCost(JoinTree* joinTreePtr);
+
+/* estimate the cost of a given Join Tree node */
+double joinTreeNodeCost(JoinTreeNode* joinTreeNodePtr);
