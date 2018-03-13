b2c48be6f8914b06356c32e60a2a753c3dea9576
Orest Gkini <orestijuve@gmail.com>
2018-03-13 12:35:14 +0200

    Added computation of every column statistics

--- quick-start-package/Joiner.cpp
+++ quick-start-package/Joiner.cpp
@@ -9,7 +9,7 @@
 #include "QueryPlan.hpp"
 #include "header.hpp"
 
-// #define time
+#define time
 
 using namespace std;
 
@@ -24,6 +24,7 @@ double timeCheckSum = 0;
 double timeConstruct = 0;
 double timeBuildPhase = 0;
 double timeProbePhase = 0;
+double timePreparation = 0;
 
 /* +---------------------+
    |The joiner functions |
@@ -586,11 +587,22 @@ int main(int argc, char* argv[]) {
         joiner.addRelation(line.c_str());
     }
 
+    #ifdef time
+    struct timeval start;
+    gettimeofday(&start, NULL);
+    #endif
+
     // Preparation phase (not timed)
     QueryPlan queryPlan;
 
     // Get the needed info of every column
     queryPlan.fillColumnInfo(joiner);
+
+    #ifdef time
+    struct timeval end;
+    gettimeofday(&end, NULL);
+    timePreparation += (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+    #endif
 
     // The test harness will send the first query after 1 second.
     QueryInfo i;
@@ -604,14 +616,12 @@ int main(int argc, char* argv[]) {
         q_counter++;
 
         #ifdef time
-        struct timeval start;
         gettimeofday(&start, NULL);
         #endif
 
         JTree *jTreePtr = treegen(&i);
 
         #ifdef time
-        struct timeval end;
         gettimeofday(&end, NULL);
         timeTreegen += (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
         #endif
@@ -630,9 +640,9 @@ int main(int argc, char* argv[]) {
         string result_str;
         uint64_t checksum = 0;
         std::vector<SelectInfo> &selections = i.selections;
-        for (size_t i = 0; i < selections.size(); i++) {
-
+        for (size_t i = 0; i < selections.size(); i++) {            
             checksum = joiner.check_sum(selections[i], result);
+
             if (checksum == 0) {
                 result_str += "NULL";
             } else {
@@ -666,6 +676,7 @@ int main(int argc, char* argv[]) {
     std::cerr << "timeTreegen: " << (long)(timeTreegen * 1000) << endl;
     std::cerr << "timeCheckSum: " << (long)(timeCheckSum * 1000) << endl;
     std::cerr << "timeConstruct: " << (long)(timeConstruct * 1000) << endl;
+    std::cerr << "timePreparation: " << (long)(timePreparation * 1000) << endl;
     flush(std::cerr);
     #endif
 
